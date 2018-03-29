53ccc08644b8c3da52716c4d2472e731e3b4861d
Gula Andrew <gyla.andrij@gmail.com>
2018-03-29 11:19:45 +0300

    fix versions

--- src/ExpertCoderSwiftmailerSendGridBundle.php
+++ src/ExpertCoderSwiftmailerSendGridBundle.php
@@ -4,6 +4,23 @@ namespace ExpertCoder\Swiftmailer\SendGridBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class ExpertCoderSwiftmailerSendGridBundle
+ * @package ExpertCoder\Swiftmailer\SendGridBundle
+ */
 class ExpertCoderSwiftmailerSendGridBundle extends Bundle
 {
+    /**
+     * Returns the bundle name that this bundle overrides.
+     *
+     * Despite its name, this method does not imply any parent/child relationship
+     * between the bundles, just a way to extend and override an existing
+     * bundle.
+     *
+     * @return string The Bundle name it overrides or null if no parent
+     */
+    public function getParent()
+    {
+        return null;
+    }
 }
--- src/Services/SendGridTransport.php
+++ src/Services/SendGridTransport.php
@@ -6,7 +6,6 @@ use finfo;
 use SendGrid;
 use Swift_Events_EventListener;
 use Swift_Mime_Attachment;
-use Swift_Mime_Message;
 use Swift_Transport;
 use Psr\Log\LoggerInterface;
 
@@ -81,12 +80,12 @@ class SendGridTransport implements Swift_Transport
     /**
      * WARNING : $failedRecipients and return value are faked.
      *
-     * @param Swift_Mime_Message $message
+     * @param \Swift_Mime_SimpleMessage $message
      * @param array              $failedRecipients
      *
      * @return int
      */
-    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    public function send(\Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
         // prepare fake data.
         $sent = 0;
@@ -187,5 +186,31 @@ class SendGridTransport implements Swift_Transport
     public function registerPlugin(Swift_Events_EventListener $plugin)
     {
         // unused
+    }
+
+    /**
+     * Check if this Transport mechanism is alive.
+     *
+     * If a Transport mechanism session is no longer functional, the method
+     * returns FALSE. It is the responsibility of the developer to handle this
+     * case and restart the Transport mechanism manually.
+     *
+     * @example
+     *
+     *   if (!$transport->ping()) {
+     *      $transport->stop();
+     *      $transport->start();
+     *   }
+     *
+     * The Transport mechanism will be started, if it is not already.
+     *
+     * It is undefined if the Transport mechanism attempts to restart as long as
+     * the return value reflects whether the mechanism is now functional.
+     *
+     * @return bool TRUE if the transport is alive
+     */
+    public function ping()
+    {
+        return true;
     }
 }
